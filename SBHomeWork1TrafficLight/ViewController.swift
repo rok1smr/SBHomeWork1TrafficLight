e61923eabfebfcfe54fa938d590dc259f89973fe
ROK1SMR <runnerbkln@gmail.com>
2021-10-20 20:13:52 +0300

    Final solution Implemented the logic of the app using simple else if loop

--- SBHomeWork1TrafficLight/ViewController.swift
+++ SBHomeWork1TrafficLight/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var actionButton: UIButton!
+    
     override func viewDidLoad() {
+        redLightView.layer.cornerRadius = 100
+        redLightView.alpha = 0.5
+        yellowLightView.layer.cornerRadius = 100
+        yellowLightView.alpha = 0.5
+        greenLightView.layer.cornerRadius = 100
+        greenLightView.alpha = 0.5
+        
+        actionButton.layer.cornerRadius = 10
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func actionButtonPressed(_ sender: Any) {
+        actionButton.setTitle("NEXT", for: .normal)
+        
+        if redLightView.alpha == 0.5 && yellowLightView.alpha == 0.5 && greenLightView.alpha == 0.5 {
+            redLightView.alpha = 1
+        } else if redLightView.alpha == 1 && yellowLightView.alpha == 0.5 && greenLightView.alpha == 0.5 {
+            redLightView.alpha = 0.5
+            yellowLightView.alpha = 1
+        } else if redLightView.alpha == 0.5 && yellowLightView.alpha == 1 && greenLightView.alpha == 0.5 {
+            yellowLightView.alpha = 0.5
+            greenLightView.alpha = 1
+        } else if redLightView.alpha == 0.5 && yellowLightView.alpha == 0.5 && greenLightView.alpha == 1 {
+            greenLightView.alpha = 0.5
+            redLightView.alpha = 1
+        }
+        
+    }
+    
 }
 
